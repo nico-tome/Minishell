0adde029da775884633a53e6b2ab0a77d67fd55b
Gaetan Janvier--De Franca <gajanvie@3G6.42angouleme.fr>
2025-12-10 12:23:29 +0100

    ajout structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntome <ntome@42angouleme.fr>               +#+  +:+       +#+        */
+/*   By: gajanvie <gajanvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 12:31:35 by ntome             #+#    #+#             */
-/*   Updated: 2025/12/10 00:36:11 by ntome            ###   ########.fr       */
+/*   Updated: 2025/12/10 11:23:57 by gajanvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,32 @@ typedef struct s_minishell
 	int				exit_status;
 	char			*pwd;
 }				t_minishell;
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	HEREDOC,
+	APPEND
+}				t_token_type;
+
+typedef struct s_token
+{
+    char				*content;
+    t_token_type		type;
+    struct s_token		*next;
+}				t_token;
+
+typedef struct s_cmd
+{
+    char			**args;
+    char			*cmd_path;
+    int				fd_in;
+    int				fd_out;
+    struct s_cmd	*next;
+}				t_cmd;
 
 char			*ms_get_prompt(t_minishell ms);
 t_prompt_params	ms_init_prompt_params(void);
