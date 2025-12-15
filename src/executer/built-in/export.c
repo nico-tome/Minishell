1365fd39f1dcb7eba6f3442cbd800e8db60b1a72
CHAT-DISPARU <gajanvie@3G2.42angouleme.fr>
2025-12-15 11:13:15 +0100

    export casi finit

--- src/executer/built-in/export.c
+++ src/executer/built-in/export.c
@@ -3,14 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: titan <titan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gajanvie <gajanvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 23:38:09 by ntome             #+#    #+#             */
-/*   Updated: 2025/12/15 00:30:02 by ntome            ###   ########.fr       */
+/*   Updated: 2025/12/15 11:13:01 by gajanvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <minishell.h>
 
 int	is_valid_export_key(char *str)
@@ -57,11 +56,69 @@ int	add_or_update_env(t_minishell *ms, char *key, char *value)
 	return (0);
 }
 
+void	print_tab_sort(char *tab)
+{
+	int	i;
+
+	i = 0;
+	printf("declare -x ");
+	while (tab[i] && tab[i] != '=')
+	{
+		printf("%c", tab[i]);
+		i++;
+	}
+	if (tab[i])
+		printf("%c", tab[i++]);
+	printf("%c", '"');
+	while (tab[i])
+	{
+		printf("%c", tab[i]);
+		i++;
+	}
+	printf("%c%c", '"', '\n');
+}
+
+char	**bubble_sort(char **tab)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = i + 1;
+		while (tab[j])
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (tab);
+}
+
 void	print_export_values(t_minishell *ms)
 {
-	(void)ms;
-	//la soit on transforme la liste en char** et c'est plus facile a sort mais faut bien gerer les free.
-	//soit on loop a travers la struct et faut réussir a sort mais là j'y arrive pas j'ai le cerveau mort mdrr.
+	char	**tab;
+	int		i;
+
+	tab = list_to_tab(ms->envp);
+	if (!tab)
+		return ;
+	tab = bubble_sort(tab);
+	i = 0;
+	while (tab[i])
+	{
+		print_tab_sort(tab[i]);
+		i++;
+	}
+	free_all(tab);
 }
 
 int	ms_export(t_minishell *ms, t_cmd *cmd)
