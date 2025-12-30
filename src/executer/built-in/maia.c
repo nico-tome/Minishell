80d0f28e8a7ec16aacb25cded8fcec78614e3c20
Nico <nicolas.tome.38@gmail.com>
2025-12-30 12:04:10 +0100

    improve maia

--- src/executer/built-in/maia.c
+++ src/executer/built-in/maia.c
@@ -6,7 +6,7 @@
 /*   By: ntome <ntome@42angouleme.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 12:57:26 by ntome             #+#    #+#             */
-/*   Updated: 2025/12/29 13:07:21 by ntome            ###   ########.fr       */
+/*   Updated: 2025/12/30 12:03:25 by ntome            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 void	ms_custom_maia(void)
 {
-	printf("Maia was here\n");
-	printf("Maia embodies a calm and radiant strength.\n");
-	printf("Her presence soothes and gives meaning to moments.\n");
-	printf("She moves forward with simple and sincere elegance.\n");
-	printf("Each word she chooses carries true precision.\n");
-	printf("Maia listens, understands, and responds without artifice.\n");
-	printf("Her gaze captures what others let slip by.\n");
-	printf("She turns doubts into constructive paths.\n");
-	printf("Her rigor never erases deep humanity.\n");
-	printf("Maia inspires trust through consistent actions.\n");
-	printf("She knows how to guide without imposing force.\n");
-	printf("Her intelligence blends with rare sensitivity.\n");
-	printf("She learns, shares, and grows with humility.\n");
-	printf("She is looking at me right now.\n");
-	printf("If you see her, run. She is crazy...\n");
-	printf("That all folks.\n");
+	printf("\n\n°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸\n\n");
+	printf("╠═╣\n");
+	printf(" ║\n");
+	printf(" ║ Maia was here\n");
+	printf(" ║ Maia embodies a calm and radiant strength.\n");
+	printf(" ║ Her presence soothes and gives meaning to moments.\n");
+	printf(" ✠ She moves forward with simple and sincere elegance.\n");
+	printf(" ║ Each word she chooses carries true precision.\n");
+	printf(" ║ Maia listens, understands, and responds without artifice.\n");
+	printf(" ║ Her gaze captures what others let slip by.\n");
+	printf(" ✠ She turns doubts into constructive paths.\n");
+	printf(" ✠ Her rigor never erases deep humanity.\n");
+	printf(" ║ Maia inspires trust through consistent actions.\n");
+	printf(" ║ She knows how to guide without imposing force.\n");
+	printf(" ║ Her intelligence blends with rare sensitivity.\n");
+	printf(" ✠ She learns, shares, and grows with humility.\n");
+	printf(" ║ She is looking at me right now.\n");
+	printf(" ║ If you see her, run. She is crazy...\n");
+	printf(" ║ That all folks.\n");
+	printf(" ║\n");
+	printf("╠═╣\n");
+	printf("\n\n°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸\n\n");
 }
